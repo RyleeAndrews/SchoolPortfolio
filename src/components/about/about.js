fe465092620947c1547005f3e20df01048a77cd6
Rylee Andrews <ryleeandrews73@gmail.com>
2018-10-21 21:23:49 -0600

    base

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -97,31 +97,101 @@ class About extends React.Component {
           </p>
           {
             this.state.slideCount === 1 ?
-              <img className="picabout" src={require('./painting1.jpg')} />
+              <img className="picabout2" src={require('./painting1.jpg')} />
               :
               null
           }
           {
             this.state.slideCount === 2 ?
-              <img className="picabout" src={require('./painting2.jpg')} />
+              <img className="picabout2" src={require('./painting2.jpg')} />
               :
               null
           }
           {
             this.state.slideCount === 3 ?
-              <img className="picabout" src={require('./painting3.jpg')} />
+              <img className="picabout2" src={require('./painting3.jpg')} />
               :
               null
           }
         </div>
         <div className="wrapper3">
           <p className="aboutP3">
-            My involvings with coding started while I was living in one of the epicenters of it all, Seattle.
+            My involvement with coding started while I was living in one of the epicenters of it all, Seattle.
             I earned my certification in the trade at Code Fellows in downtown Seattle. This oppurtunity has opened
             many doors for me creative wise and allows me to bring my thoughts to designs. I have always had
             a love for creating designs clothing wise, so the passion for bringing things to conception digitally
             is there.
           </p>
+          {
+            this.state.slideCount === 1 ?
+              <img className="picabout3" src={require('./seattle.jpg')} />
+              :
+              null
+          }
+          {
+            this.state.slideCount === 2 ?
+              <img className="picaboutdif" src={require('./seattle6.jpg')} />
+              :
+              null
+          }
+          {
+            this.state.slideCount === 3 ?
+              <img className="picabout3" src={require('./seattle3.jpg')} />
+              :
+              null
+          }
+        </div>
+        <div className="wrapper4">
+          <p className="aboutP4">
+            What sets me apart from the pack is my thirst to always evolve. I am always looking for new
+            ways to display beautiful interfaces and to keep building the very best product. Through my many trials
+            I strive to bring the best to you, with great positive energy. That positive energy is something that also
+            sets me apart.
+          </p>
+          {
+            this.state.slideCount === 1 ?
+              <img className="picabout4" src={require('./rylee.jpg')} />
+              :
+              null
+          }
+          {
+            this.state.slideCount === 2 ?
+              <img className="picabout4" src={require('./rylee2.jpg')} />
+              :
+              null
+          }
+          {
+            this.state.slideCount === 3 ?
+              <img className="picabout4" src={require('./rylee3.jpg')} />
+              :
+              null
+          }
+        </div>
+        <div className="wrapper5">
+          <p className="aboutP5">
+            I work well with others and want to see everyone grow. Working alone I set goals and achieve them.
+            Working with clients I always want to make sure they are one hundred percent involved. With full understading
+            we can work quickly and proficiently on the project. This portfolio will display what I am capable of and what
+            I can provide. Beautiful layouts and simple designs is my personal flavor of designing.
+          </p>
+          {
+            this.state.slideCount === 1 ?
+              <img className="picabout5" src={require('./me.jpg')} />
+              :
+              null
+          }
+          {
+            this.state.slideCount === 2 ?
+              <img className="picabout5" src={require('./me2.jpg')} />
+              :
+              null
+          }
+          {
+            this.state.slideCount === 3 ?
+              <img className="picabout5" src={require('./me3.jpg')} />
+              :
+              null
+          }
         </div>
       </div>
     );
