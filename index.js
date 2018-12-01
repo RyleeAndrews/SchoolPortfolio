7692df52e54064a363b05b5cba575b072a62b1d1
Rylee Andrews <ryleeandrews73@gmail.com>
2018-12-01 14:16:05 -0700

    base

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(`${__dirname}, /build`)));
+app.use(express.static((`${__dirname}/build`)));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+ '/build/src/index.html'));
 });
