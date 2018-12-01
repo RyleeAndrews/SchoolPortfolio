01474c03d5722b3667320dd9532e713287122fc1
Rylee Andrews <ryleeandrews73@gmail.com>
2018-12-01 13:44:16 -0700

    base

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(`${__dirname}/bundle/index.html`));
+app.use(express.static(`${__dirname}/bundle`));
 app.use('*', (req, res) => res.send('/'));
 
 app.listen(process.env.PORT || 8080, () => {
