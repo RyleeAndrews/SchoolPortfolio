b22296c94081b250d2d2965d2a110af1f6b2e40f
Rylee Andrews <ryleeandrews73@gmail.com>
2018-12-01 14:31:27 -0700

    base

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static((`${__dirname}/build`)));
 app.get('/about', (req, res) => {
-  res.sendFile(`${__dirname}/build/src/components/about/about.js`);
+  res.sendFile('./src/components/about/about.js');
 });
 
 app.listen(process.env.PORT || 8080, () => {
